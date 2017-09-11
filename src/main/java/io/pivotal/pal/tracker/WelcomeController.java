cc60803e3d4309b6f15d186e3f5da062296fe8fe
balajiveerappan <balaji.a.veerappan@capgemini.com>
2017-09-11 08:51:46 +0530

    Security

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return "Hello from test";
     }
 
 }
